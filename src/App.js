e9ae8717553c46ef279c79d4009676b4521df534
Sahil Kapur <sahilkapur.a@gmail.com>
2020-01-30 04:25:55 -0500

    Added v1 mockup with chart.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Doughnut } from 'react-chartjs-2';
 
 function App() {
   const [contentDisplay, updateContent] = useState("school");
@@ -68,7 +69,7 @@ function DreamBubble(props) {
   }
 
   return (
-    <div class="hoverDream" style={bubbleStyle} onClick={() => props.updateContent(props.title)} >
+    <div style={bubbleStyle} onClick={() => props.updateContent(props.title)} >
       <h5 style={labelStyle}>{props.title}</h5>
     </div>
   )
@@ -106,17 +107,144 @@ function ContentContainer(props) {
 }
 
 function SchoolContent() {
+  const graphContainerStyle = {
+    display: "flex",
+    position: "relative",
+    justifyContent: "center",
+    maxWidth: "100%",
+    flexWrap: "wrap"
+  }
+  const sectionTitleStyle = {
+    textAlign: "center",
+    paddingTop: "5%",
+    paddingBottom: "2%"
+  }
   return (
     <div>
-      school
+      <h2 style={sectionTitleStyle}>Class Attendance</h2>
+      <div style={graphContainerStyle}>
+        {DoughnutGraph({
+          title: "Econ Lecture",
+          attended: 1,
+          total: 6, 
+          percent: Math.round(1/6 * 100).toString() + "%" 
+        })}
+        {DoughnutGraph({ 
+          title: "DS Lecture", 
+          attended: 9, 
+          total: 9, 
+          percent: "100%" 
+        })}
+        {DoughnutGraph({
+          title: "Compilers Lecture",
+          attended: 7,
+          total: 9,
+          percent: "78%"
+        })}
+        {DoughnutGraph({
+          title: "HONR 399 Lecture",
+          attended: 9,
+          total: 9,
+          percent: "100%"
+        })}
+      </div>
     </div>
   );
 }
 
+function DoughnutGraph(props) {
+  const donutTitle = {
+    textAlign: "center",
+    margin: "0px",
+    marginBottom: "10px"
+  }
+  const donutContainer = {
+    marginBottom: "50px"
+  }
+  const data = {
+    labels: [
+      'Attended',
+      'Missed'
+    ],
+    datasets: [{
+      data: [props.attended, props.total - props.attended],
+      backgroundColor: [
+        '#7880FF',
+        '#d6d6d6',
+      ],
+      hoverBackgroundColor: [
+        'green',
+        '#b5b5b5',
+      ]
+    }]
+  };
+  const options = {
+    legend: {
+      display: false
+    },
+    title: {
+      text: props.percent,
+      display: false,
+      position: "bottom"
+    },
+    layout: {
+      padding: {
+        left: 0,
+        right: 0
+      }
+    },
+    responsive: true
+  }
+  return (
+    <div style={donutContainer}>
+      <h3 style={donutTitle}>{props.title}</h3>
+      <Doughnut data={data} options={options} />
+    </div>
+  )
+}
+
 function GymContent() {
+  const graphContainerStyle = {
+    display: "flex",
+    position: "relative",
+    justifyContent: "center",
+    maxWidth: "100%",
+    flexWrap: "wrap"
+  }
+  const sectionTitleStyle = {
+    textAlign: "center",
+    paddingTop: "5%",
+    paddingBottom: "2%"
+  }
   return (
     <div>
-      gym
+      <h2 style={sectionTitleStyle}>Weekly Sets</h2>
+      <div style={graphContainerStyle}>
+        {DoughnutGraph({
+          title: "Biceps",
+          attended: 3,
+          total: 20,
+          percent: Math.round(3 / 20 * 100).toString() + "%"
+        })}
+        {DoughnutGraph({
+          title: "Triceps",
+          attended: 2,
+          total: 10,
+          percent: "20%"
+        })}
+        {DoughnutGraph({
+          title: "Back",
+          attended: 4,
+          total: 15,
+          percent: "33%"
+        })}
+        {DoughnutGraph({
+          title: "Legs",
+          attended: 0,
+          total: 9,
+          percent: "0%"
+        })}
+      </div>
     </div>
   );
 }
