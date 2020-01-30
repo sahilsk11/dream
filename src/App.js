e75472715ad762ddc0a5c96d9609cfeefeb9731a
Sahil Kapur <sahilkapur.a@gmail.com>
2020-01-30 03:37:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [contentDisplay, updateContent] = useState("school");
+  const containerStyle = {
+    display: "flex",
+    width: "100%",
+    position: "relative",
+    justifyContent: "center",
+    paddingTop: "3%"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      {LeftNavBar({ updateContent })}
+      {ContentContainer({ display: contentDisplay })}
+    </div>
+  );
+}
+
+function LeftNavBar(props) {
+  const navStyle = {
+    height: "90vh",
+    width: "20%",
+    backgroundColor: "white",
+    margin: "5%",
+    marginTop: "0px"
+  };
+  const title = {
+    marginTop: "20px",
+    marginBottom: "0px",
+    textAlign: "center",
+    backgroundColor: "white",
+  }
+  const iconStyle = {
+    margin: "0px auto",
+    display: "block",
+    width: "50%",
+    marginTop: "20px",
+    marginBottom: "40px",
+    maxWidth: "90px"
+  }
+  return (
+    <div style={navStyle}>
+      <h1 style={title}>DREAM</h1>
+      <img style={iconStyle} src="./dream.png" />
+      {DreamBubble({ title: "school", updateContent: props.updateContent })}
+      {DreamBubble({ title: "gym", updateContent: props.updateContent })}
+      {DreamBubble({ title: "health", updateContent: props.updateContent })}
+      {DreamBubble({ title: "games", updateContent: props.updateContent })}
+      {/*DreamBubble({ title: "privacy" })*/}
+    </div>
+  );
+}
+
+function DreamBubble(props) {
+  let bubbleStyle = {
+    border: "3px solid black",
+    width: "100px",
+    height: "40px",
+    display: "block",
+    margin: "0px auto",
+    marginBottom: "60px",
+    cursor: "pointer",
+  }
+  const labelStyle = {
+    textAlign: "center",
+    margin: "12px",
+  }
+
+  return (
+    <div class="hoverDream" style={bubbleStyle} onClick={() => props.updateContent(props.title)} >
+      <h5 style={labelStyle}>{props.title}</h5>
+    </div>
+  )
+}
+
+function ContentContainer(props) {
+  const containerWrapperStyle = {
+    width: "80%",
+    minHeight: "90vh"
+  }
+  const pageContentStyle = {
+    backgroundColor: "white",
+    height: "90%",
+    margin: "5%",
+    marginTop: "0px"
+  }
+  function renderContentDisplay() {
+    if (props.display == "school") {
+      return SchoolContent();
+    } else if (props.display == "gym") {
+      return GymContent();
+    } else if (props.display == "health") {
+      return GymContent();
+    } else if (props.display == "games") {
+      return GymContent();
+    }
+  }
+  return (
+    <div style={containerWrapperStyle}>
+      <div style={pageContentStyle}>
+        {renderContentDisplay()}
+      </div>
+    </div>
+  )
+}
+
+function SchoolContent() {
+  return (
+    <div>
+      school
+    </div>
+  );
+}
+
+function GymContent() {
+  return (
+    <div>
+      gym
     </div>
   );
 }
