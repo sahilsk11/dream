a77b3ba6044323ab9a0bba02f81486ee0f13ddc7
Sahil Kapur <sahilkapur.a@gmail.com>
2020-09-13 04:59:50 -0700

    add build

---
+++ frontend/build/precache-manifest.4cb5135ac67b9164f94f542d3fa29a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12a9827c2dcf8d8133f23cb0f3017ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eff342ba29456ea1ba4e",
+    "url": "./static/css/main.fd30a25f.chunk.css"
+  },
+  {
+    "revision": "9795fc7d257dedebfd78",
+    "url": "./static/js/2.6f99f304.chunk.js"
+  },
+  {
+    "revision": "2a6f7b0ac760b5d15fb8b482e9b9b618",
+    "url": "./static/js/2.6f99f304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff342ba29456ea1ba4e",
+    "url": "./static/js/main.e690ce3d.chunk.js"
+  },
+  {
+    "revision": "0e47234f8f7657ad856a",
+    "url": "./static/js/runtime-main.d9529578.js"
+  }
+]);
